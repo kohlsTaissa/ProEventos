ea220634021a4a67e1e7cd08f8fe9c32391ac9a4
Taissa <kohlstaissa@gmail.com>
2025-05-07 16:29:30 -0300

    Seção 8: Angular pt2 - Reactive forms e mudança de layout

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+    get f():any{
+      return this.form.controls;
+    }
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmaSenha')
+    };
+
+    this.form = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      descricao: ['', Validators.required],
+      userName: ['', Validators.required],
+      senha: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
+      confirmaSenha: ['', Validators.required]
+    }, formOptions);
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+  }
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
   }
 
 }
--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  get f():any{
+    return this.form.controls;
+  }
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmaSenha')
+    };
+
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      userName: ['', Validators.required],
+      senha: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
+      confirmaSenha: ['', Validators.required]
+    }, formOptions);
   }
 
 }
